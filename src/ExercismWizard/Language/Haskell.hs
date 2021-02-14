f7f2dbc621df7b728f05d132fcc9eec3b3808e43
Javran Cheng <Javran.C@gmail.com>
2021-02-13 22:06:06 -0800

    simplify output.

--- src/ExercismWizard/Language/Haskell.hs
+++ src/ExercismWizard/Language/Haskell.hs
@@ -5,18 +5,32 @@ module ExercismWizard.Language.Haskell
   )
 where
 
+import qualified Control.Foldl as Foldl
 import Control.Monad
+import Data.Monoid
+import qualified Data.Text as T
 import ExercismWizard.FSPath
 import ExercismWizard.Types
+import System.Exit
 import Turtle.Pattern
 import Turtle.Prelude
 import Turtle.Shell
 
 runOrmolu :: ExercismCli -> Exercise -> IO ()
-runOrmolu _ _ = sh $ do
-  cwd <- pwd
-  forM_ ["src", "test"] $ \s -> do
-    fn <- find (suffix ".hs") (cwd </> s)
-    liftIO $ putStrLn $ "Formating: " <> show (toText fn)
-    _ <- proc "ormolu" ["--mode=inplace", toText fn] ""
-    pure ()
+runOrmolu _ _ = do
+  t <-
+    getSum
+      <$> (reduce (Foldl.mconcat) $ do
+             cwd <- pwd
+             mconcat
+               <$> (forM ["src", "test"] $ \s -> do
+                      fn <- find (suffix ".hs") (cwd </> s)
+                      ec <- proc "ormolu" ["--mode=inplace", toText fn] ""
+                      if ec == ExitSuccess
+                        then pure (1 :: Sum Int)
+                        else do
+                          liftIO $
+                            putStrLn $
+                              "Failure when formating " <> T.unpack (toText fn) <> ", " <> show ec
+                          pure mempty))
+  putStrLn $ "Formatted " <> show t <> " file(s)."
