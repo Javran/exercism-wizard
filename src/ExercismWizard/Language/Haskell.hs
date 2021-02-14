09ac3ef722891a42d14c8c9912edb300331fc9ac
Javran Cheng <Javran.C@gmail.com>
2021-02-13 21:35:49 -0800

    limit to just src/ and test/

--- src/ExercismWizard/Language/Haskell.hs
+++ src/ExercismWizard/Language/Haskell.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
-module ExercismWizard.Language.Haskell
-   (
- runOrmolu
-   ) where
 
-import ExercismWizard.Types
-import Turtle.Prelude
-import Turtle.Pattern
-import Turtle.Shell
+module ExercismWizard.Language.Haskell
+  ( runOrmolu
+  )
+where
+
+import Control.Monad
 import ExercismWizard.FSPath
+import ExercismWizard.Types
+import Turtle.Pattern
+import Turtle.Prelude
+import Turtle.Shell
 
 runOrmolu :: ExercismCli -> Exercise -> IO ()
 runOrmolu _ _ = sh $ do
-  fn <- find (suffix ".hs") =<< pwd
-  liftIO $ putStrLn $ "Formating: " <> show (toText fn)
-  _ <- proc "ormolu" ["--mode=inplace", toText fn] ""
-  pure ()
+  cwd <- pwd
+  forM_ ["src", "test"] $ \s -> do
+    fn <- find (suffix ".hs") (cwd </> s)
+    liftIO $ putStrLn $ "Formating: " <> show (toText fn)
+    _ <- proc "ormolu" ["--mode=inplace", toText fn] ""
+    pure ()
