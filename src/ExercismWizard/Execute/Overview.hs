1628a4e684bd71c47dbad7386cde3d159e97a2b1
Javran Cheng <Javran.C@gmail.com>
2021-03-06 23:37:05 -0800

    fetch track names.

--- src/ExercismWizard/Execute/Overview.hs
+++ src/ExercismWizard/Execute/Overview.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE NamedFieldPuns #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module ExercismWizard.Execute.Overview
   ( getOverview
   )
 where
 
+import Control.Arrow
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Map.Strict as M
 import Data.Maybe
-import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Data.Time.Clock
 import qualified ExercismWizard.Config as EWConf
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import Text.XML.HXT.Core
 import Web.Cookie
 
 getOverview :: IO ()
@@ -36,4 +40,18 @@ getOverview = do
       req = initReq {cookieJar = Just cj}
   -- TODO: impl
   resp <- httpLbs req mgr
-  print (responseBody resp)
+  let raw = T.unpack $ decodeUtf8 $ BSL.toStrict $ responseBody resp
+  result <-
+    runX $
+      readString [withParseHTML yes, withWarnings no] raw
+        >>> deep (hasName "div" >>> hasAttrValue "class" (== "joined-tracks"))
+        /> hasName "div"
+        >>> hasAttrValue "class" (("tracks" `elem`) . words)
+        >>> deep (hasName "a" >>> hasAttrValue "class" (== "track joined"))
+        >>> getChildren
+        >>> (hasName "div" >>> hasAttrValue "class" (== "title"))
+        >>> getChildren
+        >>> getChildren
+        >>> getText
+
+  mapM_ print result
